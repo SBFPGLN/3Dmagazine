75d1b77aef17a34af2cdcd4d7b78ca7f23c8a327
SBFPGLN <2718544215@qq.com>
2022-09-19 15:59:11 +0800

    SBFPGLN first commit

---
+++ js/bookImgData.js
@@ -0,0 +1,176 @@
+// 书本目录，如果无ols变量则无目录，ols 变量名勿删，全局共享
+var ols = [
+  {
+    "caption": "封面",
+    "page": "1",
+  },
+  {
+    "caption": "合约基本",
+    "page": "2",
+  },
+  {
+    "caption": "状态变量",
+    "page": "3",
+  },
+  {
+    "caption": "状态变量",
+    "page": "4",
+  },
+  {
+    "caption": "状态变量",
+    "page": "5",
+  },
+  {
+    "caption": "数字运算",
+    "page": "6",
+  },
+  {
+    "caption": "时间单位",
+    "page": "7",
+  },
+  {
+    "caption": "结构体",
+    "page": "8",
+  },
+  {
+    "caption": "结构体",
+    "page": "9",
+  },
+  {
+    "caption": "数组",
+    "page": "10",
+  },
+  {
+    "caption": "数组",
+    "page": "11",
+  },
+  {
+    "caption": "映射（Mapping）",
+    "page": "12",
+  },
+  {
+    "caption": "变量数据存储和作用域",
+    "page": "13",
+  },
+  {
+    "caption": "变量数据存储和作用域",
+    "page": "14",
+  },
+  {
+    "caption": "keccak256和类型转换",
+    "page": "15",
+  },
+  {
+    "caption": "keccak256和类型转换",
+    "page": "16",
+  },
+  {
+    "caption": "常量",
+    "page": "17",
+  },
+  {
+    "caption": "函数",
+    "page": "18",
+  },
+  {
+    "caption": "函数",
+    "page": "19",
+  },
+  {
+    "caption": "函数",
+    "page": "20",
+  },
+  {
+    "caption": "函数",
+    "page": "21",
+  },
+  {
+    "caption": "构造函数和修饰器",
+    "page": "22",
+  },
+  {
+    "caption": "构造函数和修饰器",
+    "page": "23",
+  },
+  {
+    "caption": "构造函数和修饰器",
+    "page": "24",
+  },
+  {
+    "caption": "事件",
+    "page": "25",
+  },
+  {
+    "caption": "异常",
+    "page": "26",
+  },
+  {
+    "caption": "继承",
+    "page": "27",
+  },
+  {
+    "caption": "继承",
+    "page": "28",
+  },
+  {
+    "caption": "继承",
+    "page": "29",
+  },
+  {
+    "caption": "抽象合约和接口",
+    "page": "30",
+  },
+  {
+    "caption": "抽象合约和接口",
+    "page": "31",
+  },
+  {
+    "caption": "抽象合约和接口",
+    "page": "32",
+  },
+  {
+    "caption": "函数重载",
+    "page": "33",
+  },
+  {
+    "caption": "库合约",
+    "page": "34",
+  },
+  {
+    "caption": "库合约",
+    "page": "35",
+  },
+  {
+    "caption": "Import",
+    "page": "36",
+  },
+  {
+    "caption": "接受ETH",
+    "page": "37",
+  },
+  {
+    "caption": "发送ETH",
+    "page": "38",
+  },
+  {
+    "caption": "发送ETH",
+    "page": "39",
+  },
+  {
+    "caption": "发送ETH",
+    "page": "40",
+  },
+  {
+    "caption": "封底",
+    "page": "41",
+  },
+
+];
+
+// 路径配置
+var loadImgpath = "./files/thumb/"
+
+bookConfig.largePath = loadImgpath    // 大图路径
+bookConfig.normalPath = loadImgpath
+bookConfig.thumbPath = loadImgpath
+bookConfig.totalPageCount = ols.length    // 页面数量
